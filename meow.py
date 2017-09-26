69d43f43d1cf78a5796187b117ffc0b1e057cfb5
Студент НИУ ВШЭ <student@BASM-402-STD20.staff.corp.local>
2017-09-26 11:49:47 +0300

    message

---
+++ meow.py
@@ -0,0 +1,61 @@
+num1= (input('Введите число1: '))
+
+#word_length=len(x)
+#print(word_length)
+'''
+if num!=0:
+    print(num, num*10)
+else:
+    #num=num+100
+    num+=100
+print (num)
+
+
+if num>0:
+    print (num)
+    print ('bolshe nulya')
+else:
+    print ('ne bolshe nulya')
+    if num>-2:
+           print ('bolshe -2')
+    else:
+        print ('ne bolshe -2')
+print (num)
+
+
+
+if num>0 or num <=5:
+    print ('num строго больше нуля или меньше или равно пяти')
+    
+
+if True:
+    print ('hello')
+else:
+    print ('noooo')
+
+
+if num1%2==0 and num2%2==0:
+    print ('oni chetnye!!')
+elif num1%2!=0 and num2%2==0:
+    print ('num1 nechetnoye, a num2 - chetnoye')
+elif num1%2==0 and num2%2!=0:
+    print ('num1 chetnoye, a num2 - nechetnoye')
+else:
+    print ('oba nechethye')
+
+
+if num1>0:
+    print (1)
+elif num1==0:
+    print (0)
+else:
+    print (-1)
+
+    
+if (num1%4==0 and num1%100!=0) or (num1%400==0):
+    print ('on visokosen !!!!!')
+'''
+
+
+
+
