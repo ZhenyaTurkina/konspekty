0e6d5f7a9c104647d309c7544c82f4d162e4152c
ZhenyaTurkina <31654808+ZhenyaTurkina@users.noreply.github.com>
2017-11-09 11:51:12 +0300

    Add files via upload

---
+++ stroki.py
@@ -0,0 +1,49 @@
+'''guests = ["Анна", "Боб", "Кейт"]
+
+fib = []
+
+for n in range (5):
+    print (n)
+    fib.append (n)
+    print (fib)
+print (fib)
+print (len (fib))
+letters = list('abracadabra')# элементы этого списка — элементы исходной строки
+
+my_empty_list = []    # пустой список
+
+teams = ['team_A', 'snakes_team', ['team_B_1', 'team_B_2'], 'СamelsTeam'] 
+'''
+'''
+text = 'дочь ночь полюшко латынюшка'
+words=text.split()
+
+for i in (words):
+    print ([: :2])
+'''
+'''
+nums=[[1,2,3]]*3
+print (nums)
+
+for row in nums:
+    #print (row)
+    #print ('inner')
+    for n in row:
+        print (n, end=' ')
+    print ()
+
+'''
+'''
+w='word'
+letters=[]
+for i in w:
+    letters.append(i)
+print (letters)
+'''
+
+nums=[1,2,3]
+nums_str=''
+for d in nums:
+    nums_str = nums_str + '' + str(d)
+print (nums_str)
+
---
+++ stroki_cons.py
@@ -0,0 +1,274 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+>>> guests
+['Анна', 'Боб', 'Кейт']
+>>> fib
+[0, 1, 1, 2, 3, 5, 8, 13]
+>>> letters
+['a', 'b', 'r', 'a', 'c', 'a', 'd', 'a', 'b', 'r', 'a']
+>>> guests.append ('Lala')
+>>> guests
+['Анна', 'Боб', 'Кейт', 'Lala']
+>>> guests [3]
+'Lala'
+>>> guests [-1]
+'Lala'
+>>> guests [-1]
+'Lala'
+>>> [-1][0]
+-1
+>>> guests [-1][0]
+'L'
+>>> 
+>>> guests [-1][1]
+'a'
+>>> [1,2,3]*2
+[1, 2, 3, 1, 2, 3]
+>>> n=[[0]*3]*2
+>>> n
+[[0, 0, 0], [0, 0, 0]]
+>>> guests [-10]
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    guests [-10]
+IndexError: list index out of range
+>>> guests [-3]
+'Боб'
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 6, in <module>
+    print [n]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 6, in <module>
+    print [n]
+TypeError: 'builtin_function_or_method' object is not subscriptable
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+[0, 1, 1, 2, 3, 5, 8, 13, 0, 1, 2, 3, 4]
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+[0, 1, 1, 2, 3, 5, 8, 13, 0, 1, 2, 3, 4]
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 9, in <module>
+    print (len.fib)
+AttributeError: 'builtin_function_or_method' object has no attribute 'fib'
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+[0, 1, 1, 2, 3, 5, 8, 13, 0, 1, 2, 3, 4]
+13
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+1
+2
+3
+4
+[0, 1, 2, 3, 4]
+5
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+0
+[0]
+1
+[0, 1]
+2
+[0, 1, 2]
+3
+[0, 1, 2, 3]
+4
+[0, 1, 2, 3, 4]
+[0, 1, 2, 3, 4]
+5
+>>> fib
+[0, 1, 2, 3, 4]
+>>> guests
+['Анна', 'Боб', 'Кейт']
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+['дочь', 'ночь', 'полюшко', 'латынюшка']
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+['дочь', 'ночь', 'полюшко', 'латынюшка']
+ночь
+['дочь', 'ночь']
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+['дочь', 'ночь', 'полюшко', 'латынюшка']
+ночь
+['дочь', 'ночь']
+['полюшко', 'латынюшка']
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 28, in <module>
+    for i in range (nums):
+TypeError: 'list' object cannot be interpreted as an integer
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 30, in <module>
+    print (n)
+NameError: name 'n' is not defined
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+[1, 2, 3]
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+inner
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 31, in <module>
+    print (n)
+NameError: name 'n' is not defined
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+inner
+1
+2
+3
+inner
+1
+2
+3
+inner
+1
+2
+3
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+[1, 2, 3]
+inner
+1
+2
+3
+[1, 2, 3]
+inner
+1
+2
+3
+[1, 2, 3]
+inner
+1
+2
+3
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+123123123
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+123
+123
+123
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+[[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+1 2 3 
+1 2 3 
+1 2 3 
+>>> w='word'
+>>> [].append('w')
+>>> n=[]
+>>> n.append('w')
+>>> n
+['w']
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+Traceback (most recent call last):
+  File "C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py", line 40, in <module>
+    letters.append(j)
+NameError: name 'j' is not defined
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+['w', 'o', 'r', 'd']
+>>> i=list(w)
+>>> i
+['w', 'o', 'r', 'd']
+>>> str(1)
+'1'
+>>> str (r)
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    str (r)
+NameError: name 'r' is not defined
+>>> str(l)
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    str(l)
+NameError: name 'l' is not defined
+>>> l=list(w)
+>>> l
+['w', 'o', 'r', 'd']
+>>> str(1)
+'1'
+>>> nums=[1,2,3]
+>>> '1 2 3'
+'1 2 3'
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+123
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+1 2 3 
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+123
+>>> 
+== RESTART: C:/Users/student/AppData/Local/Programs/Python/Python35-32/у.py ==
+1123
+>>> 
