ffdc4f873043551e2b1bbec809e5ea11856503b9
ZhenyaTurkina <31654808+ZhenyaTurkina@users.noreply.github.com>
2017-12-06 20:31:23 +0300

    Create lists1.py

---
+++ lists/lists1.py
@@ -0,0 +1,17 @@
+word = (input ("Введите слово: "))
+
+a = len(word)
+
+print (word)
+
+if a == 0:
+    print ("Ваша строка пуста")
+else:
+    for m in range (0, a):
+        print (word[:-1])
+        word = word[:-1]
+        
+        
+'''
+по убыванию
+'''
